36f61c567f3fe8807f5152aa5cffc751f590a047
Mark Small <mark.small@telespazio.com>
2024-07-12 22:39:21 +0100

    feat(devops): Add ability to build releases
    I've added dependencies and config to build GitHub releases of the
    app. In practice what this means is:
    
    1. Analyze commit history to determine what the version should be.
       There is no human interaction on this, so the version is generated
       systematically, based on whether the commits have new features,
       bugs or breaking changes.
    2. Generate a CHANGELOG file to indicate the commits involved in each
       release. This file is also used in the `releases` page on GitHub,
       it is auto-generated and shouldn't be edited by hand.
    3. Update the package.json file to set the new version attribute.
    4. Create tag using the version attribute, prefixed with a `v` e.g.
       `v1.1.1`.
    5. Push changes to Github.
    
    See https://semantic-release.gitbook.io/semantic-release for more on
    the semantic-release tool.

---
+++ release.config.mjs
@@ -0,0 +1,24 @@
+const config = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+  preset: 'angular',
+  branches: ['main'],
+};
+
+export default config;
