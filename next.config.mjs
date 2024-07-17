325a3e91f8392cb7af073d41414de34eb41d99e7
Mark Small <mark.small@astrosat.net>
2024-07-17 17:04:53 +0100

    feat(project): Configure app to run on base path
    Since most of the apps run on the same hostname, each app needs to
    run on a different path. This is an easy way to define said path.
    In the template it is set to `template-app`, for each app built from
    this template, the dev just needs to configure the text for the
    path.

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,12 @@ const nextConfig = {
   // creates a `standalone` folder in .next folder, including script within
   // it called `server.js`, which needs to be run by Dockerfile in CMD.
   output: 'standalone',
+  // assetPrefix: follows the same rules as deployPath
+  assetPrefix: '/template-app',
+  basePath: '/template-app',
   env: {
     appVersion: pkg.version, // Provide version to app, so it can be displayed.
+    deployPath: '/template-app',
   },
 };
 
